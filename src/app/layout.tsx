2f344bc5435c26a433129b32abc987fa696ebceb
fortan <fortanpireva99@gmail.com>
2024-01-08 17:57:54 +0100

    add google analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { useEffect } from 'react'
+import { initGA, logPageView } from '@/utils/ga-analytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +16,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    initGA();
+    logPageView();
+  }, [])
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
