e9ae17394c3f11a1f77fbe27a79c265c7cfeebdc
fortan <fortanpireva99@gmail.com>
2024-01-08 18:46:16 +0100

    add entry

--- src/hooks/useFeatureFlag.ts
+++
@@ -1,8 +0,0 @@
-// hooks/useFeatureGate.ts
-import featureFlags from '../utils/featureFlags';
-
-export function useFeatureFlag(feature: keyof typeof featureFlags) {
-    // In a real-world scenario, you might want to check user roles, 
-    // subscription levels, or other criteria here
-    return featureFlags[feature];
-}
--- src/utils/featureFlags.ts
+++
@@ -1,7 +0,0 @@
-// utils/featureFlags.ts
-const featureFlags = {
-    newDashboard: false, // Set to true to enable the new dashboard
-    betaFeatures: false, // Set to true to enable beta features
-};
-
-export default featureFlags;
