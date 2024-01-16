01404f82fc35ebb863361209ea5b4869a618819d
fortan <fortanpireva99@gmail.com>
2024-01-16 14:03:21 +0100

    add navbar and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import Footer from '@/components/footer/Footer'
 import Hero from '@/components/hero/Hero'
+import Navbar from '@/components/navbar/navbar'
 import Pricing from '@/components/pricing/pricing'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar />
       <Hero />
-
       <Pricing />
+      <Footer />
     </main>
   )
 }
