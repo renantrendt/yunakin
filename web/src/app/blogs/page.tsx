6d36501090357328932dd7c6bf146fb3bb105035
fortan <fortanpireva99@gmail.com>
2024-01-23 14:07:35 +0100

    fetch blog data from strapi

--- web/src/app/blogs/page.tsx
+++ web/src/app/blogs/page.tsx
@@ -12,79 +12,12 @@ interface Meta {
     };
 }
 
-const blogs = [
-    {
-        "title": "Exploring the World of Coffee: A Journey from Bean to Cup",
-        "description": "Dive into the fascinating journey of coffee, exploring its origins, the art of roasting, and the craft of brewing the perfect cup. Discover the diverse cultures and traditions that surround coffee consumption around the globe.",
-        "lastUpdated": "2024-01-20",
-        "authorName": "Alex Johnson",
-        "authorImageUrl": "https://example.com/images/authors/alex-johnson.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/coffee-journey.jpg"
-    },
-    {
-        "title": "The Future of Renewable Energy: Innovations and Challenges",
-        "description": "An in-depth look at the cutting-edge innovations driving the future of renewable energy. Explore the potential solutions to the challenges facing the adoption of solar, wind, and other renewable energy sources.",
-        "lastUpdated": "2024-01-18",
-        "authorName": "Samantha Lee",
-        "authorImageUrl": "https://example.com/images/authors/samantha-lee.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/renewable-energy-future.jpg"
-    },
-    {
-        "title": "A Guide to Sustainable Living: Simple Steps for a Greener Lifestyle",
-        "description": "Embark on a journey towards sustainability with practical tips for reducing your environmental footprint. From eco-friendly home practices to sustainable travel, learn how you can make a difference.",
-        "lastUpdated": "2024-01-15",
-        "authorName": "Michael Brown",
-        "authorImageUrl": "https://example.com/images/authors/michael-brown.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/sustainable-living-guide.jpg"
-    },
-    {
-        "title": "The Evolution of Digital Art: From Pixels to Masterpieces",
-        "description": "Discover the fascinating evolution of digital art, tracing its history from early pixel art to contemporary digital masterpieces. Learn about the tools and technologies that have shaped this dynamic form of expression.",
-        "lastUpdated": "2024-01-10",
-        "authorName": "Emily Carter",
-        "authorImageUrl": "https://example.com/images/authors/emily-carter.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/digital-art-evolution.jpg"
-    },
-    {
-        "title": "A Guide to Sustainable Living: Simple Steps for a Greener Lifestyle",
-        "description": "Embark on a journey towards sustainability with practical tips for reducing your environmental footprint. From eco-friendly home practices to sustainable travel, learn how you can make a difference.",
-        "lastUpdated": "2024-01-15",
-        "authorName": "Michael Brown",
-        "authorImageUrl": "https://example.com/images/authors/michael-brown.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/sustainable-living-guide.jpg"
-    },
-    {
-        "title": "The Evolution of Digital Art: From Pixels to Masterpieces",
-        "description": "Discover the fascinating evolution of digital art, tracing its history from early pixel art to contemporary digital masterpieces. Learn about the tools and technologies that have shaped this dynamic form of expression.",
-        "lastUpdated": "2024-01-10",
-        "authorName": "Emily Carter",
-        "authorImageUrl": "https://example.com/images/authors/emily-carter.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/digital-art-evolution.jpg"
-    }, {
-        "title": "A Guide to Sustainable Living: Simple Steps for a Greener Lifestyle",
-        "description": "Embark on a journey towards sustainability with practical tips for reducing your environmental footprint. From eco-friendly home practices to sustainable travel, learn how you can make a difference.",
-        "lastUpdated": "2024-01-15",
-        "authorName": "Michael Brown",
-        "authorImageUrl": "https://example.com/images/authors/michael-brown.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/sustainable-living-guide.jpg"
-    },
-    {
-        "title": "The Evolution of Digital Art: From Pixels to Masterpieces",
-        "description": "Discover the fascinating evolution of digital art, tracing its history from early pixel art to contemporary digital masterpieces. Learn about the tools and technologies that have shaped this dynamic form of expression.",
-        "lastUpdated": "2024-01-10",
-        "authorName": "Emily Carter",
-        "authorImageUrl": "https://example.com/images/authors/emily-carter.jpg",
-        "blogImageUrl": "https://example.com/images/blogs/digital-art-evolution.jpg"
-    }
-]
-
 const BlogPage = () => {
-    //eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [meta, setMeta] = useState<Meta | undefined>();
-    //eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [data, setData] = useState<any>([]);
-    //eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [isLoading, setLoading] = useState(true);
+    const [blogs, setBlogs] = useState<any[]>([]);
+
     const fetchData = useCallback(async (start: number, limit: number) => {
         setLoading(true);
         try {
@@ -125,23 +58,23 @@ const BlogPage = () => {
         fetchData(0, Number(process.env.NEXT_PUBLIC_PAGE_LIMIT));
     }, [fetchData]);
 
+
+
     return (
         <div className='mb-24'>
             <PageHeader heading="Our Blog" text="Checkout Something Cool" />
             <div className='grid grid-cols-12 justify-items-center w-full gap-x-4 px-4 mx-auto gap-y-12'>
-
-                {blogs.map((blog, index) => (
+                {data && data.map((blog: any, index: any) => (
                     <Link key={index} className="card w-full  max-w-lg bg-base-100 shadow-xl col-span-12  md:col-span-6 lg:col-span-4 hover:scale-110 hover:cursor-pointer 
-                    transition duration-150" href={`/blogs/${blog.title.replaceAll(" ", "-")}`}>
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    transition duration-150" href={`/blogs/${blog.attributes.title.replaceAll(" ", "-")}`}>
+                        <figure><img src={"https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"} alt="Shoes" /></figure>
                         <div className="card-body pb-4">
-                            <h2 className="card-title">{blog.title}</h2>
-                            <p>{blog.description}</p>
+                            <h2 className="card-title">{blog.attributes.title}</h2>
+                            <p>{blog.attributes.description[0].children[0].text}</p>
                         </div>
                     </Link>
                 ))}
             </div>
-
         </div>
     )
 }
