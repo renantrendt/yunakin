9c1b73762e8347596160fe7b7ee8153d38f6f994
fortan <fortanpireva99@gmail.com>
2024-01-24 09:51:00 +0100

    implement testimonial component

--- web/src/app/(landing)/page.tsx
+++ web/src/app/(landing)/page.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 import Hero from '@/components/hero/Hero'
 import Pricing from '@/components/pricing/pricing'
 import FAQ from '@/components/faq/FAQ'
+import Testimonial from '@/components/testimonial/Testimonial'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col  mx-auto p-2 md:px-24">
       <Hero />
+      <Testimonial />
       <Pricing />
       <FAQ />
     </main>
