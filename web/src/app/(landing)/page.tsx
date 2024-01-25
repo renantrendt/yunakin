1d4d1a96ae3765cc28ff1952492defa2670114a0
fortan <fortanpireva99@gmail.com>
2024-01-25 22:38:16 +0100

    add features component

--- web/src/app/(landing)/page.tsx
+++ web/src/app/(landing)/page.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 import Hero from '@/components/hero/Hero'
 import FAQ from '@/components/faq/FAQ'
 import Testimonial from '@/components/testimonial/Testimonial'
+import Features from '@/components/features/features'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col  mx-auto p-2 md:px-24">
       <Hero />
+      <Features />
       <Testimonial />
       <FAQ />
     </main>
--- web/src/app/not-found.tsx
+++ web/src/app/not-found.tsx
@@ -6,25 +6,13 @@ const NotFoundPage = () => {
 
         <div className="flex items-center justify-center min-h-screen bg-white py-48">
             <div className="flex flex-col">
-                <span className="text-center font-bold my-10 opacity-30">
-                    MDI (npm i @mdi/font) reuqired for all icons
-
-                    <Link href="https://egoistdeveloper.github.io/twcss-to-sass-playground/" target="_blank" className="text-blue-600">
-                        Convetert to SASS
-                    </Link>
-                </span>
-
                 <div className="flex flex-col items-center">
-                    <div className="text-indigo-500 font-bold text-7xl">
+                    <div className="text-black font-bold text-5xl md:text-[200px] underline  decoration-8 underline-offset-0	    	">
                         404
                     </div>
 
-                    <div className="font-bold text-3xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
-                        This page does not exist
-                    </div>
-
-                    <div className="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
-                        The page you are looking for could not be found.
+                    <div className=" text-3xl lg:text-3xl md:text-2xl mt-10">
+                        Page not found
                     </div>
                 </div>
             </div>
