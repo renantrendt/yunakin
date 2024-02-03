8c2e9f04de4950934c152dd1c208749be746c01b
fortan <fortanpireva99@gmail.com>
2024-02-03 07:43:49 +0100

    add usedby section

--- web/src/app/(landing)/page.tsx
+++ web/src/app/(landing)/page.tsx
@@ -6,22 +6,14 @@ import FAQ from '@/components/section/faq/FAQ'
 import Testimonial from '@/components/section/testimonial/Testimonial'
 import Features from '@/components/features/features'
 import Cta from '@/components/section/cta/cta'
-import { InfiniteMovingCards } from '@/components/atomic/infinite-moving-cards'
+import UsedBy from '@/components/section/usedBy/UsedBy'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen  flex-col  mx-auto px-4 md:px-28 max-w-8xl">
       <Hero />
       {/* <TrustedBy /> */}
-      <InfiniteMovingCards items={
-        [
-          {
-            name: 'John Doe',
-            title: 'CEO, Company',
-            quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          }
-        ]
-      } />
+      <UsedBy />
       <Cta />
       <Features />
       <Testimonial />
