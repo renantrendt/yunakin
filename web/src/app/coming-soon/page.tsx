06b611e4bd5a515f0c4c5752afc358a17c904cad
fortan <fortanpireva99@gmail.com>
2024-02-08 16:08:10 +0100

    finish coming soon integration

--- web/src/app/coming-soon/page.tsx
+++ web/src/app/coming-soon/page.tsx
@@ -1,12 +1,66 @@
+'use client'
 import React from 'react'
 
 import ComingSoonIcon from '@/assets/icons/coming-soon/icon'
 import localFont from 'next/font/local'
 import { cn } from '@/utils/cn';
+import getClientSideQueryClient from '@/react-query/client/queryClient';
 
 const uniSans = localFont({ src: '../fonts/uni-sans.heavy-caps.otf' });
 const monaSans = localFont({ src: '../fonts/Mona-Sans-Light.otf' });
+
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import InputField from '@/components/atomic/input/InputField';
+import { useMutation } from '@tanstack/react-query';
+import customToast from '@/components/atomic/toast/customToast';
+import LoadingIcon from '@/assets/icons/LoadingIcon';
+
+const schema = yup.object({
+    email: yup.string().email().required(),
+})
+
+interface FormValues {
+    email: string
+}
 const ComingSoon = () => {
+
+    const addEmailToWaitinListMutation = useMutation({
+        mutationFn: async (email: string) => {
+            const response = await fetch('/api/waiting-list', {
+                method: 'POST',
+                body: JSON.stringify({ email }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                cache: 'no-cache'
+            })
+            if (!response.ok) {
+                if (response.status === 400) {
+                    const data = await response.json()
+                    throw new Error(data.message)
+                }
+                throw new Error('Something bad happened')
+            }
+            return response.json()
+        },
+        onSuccess: () => {
+            customToast.success("Successfully added to waiting list. We'll notify you when we launch")
+        },
+        onError: (error) => {
+            customToast.error(error.message)
+        }
+    })
+    const { handleSubmit, control, formState: { errors } } = useForm<FormValues>(
+        {
+            resolver: yupResolver(schema)
+        }
+    )
+
+    const onSubmit = async (data: any) => {
+        await addEmailToWaitinListMutation.mutateAsync(data.email);
+    }
     return (
         <div className={cn("w-full h-screen coming-soon-linear-gradient ", monaSans.className)}
         >
@@ -16,20 +70,29 @@ const ComingSoon = () => {
                 </div>
                 <h1 className={cn("uppercase text-center text-white text-[38px] lg:text-8xl font-black", uniSans.className, "font-black")}>launching soon</h1>
                 <p className="text-[#B2AFD0] text-xs   text-center lg:mt-3 lg:text-[29px] font-light ">The Ultimate SaaS Starter Kit with all you need to ship fast.</p>
-                <div className='flex flex-col w-full justify-center items-center lg:pt-[60px] pt-10 gap-0 lg:gap-5 '>
-                    <div className='relative flex  items-center w-10/12 sm:w-7/12 md:w-5/12  lg:w-[600px]'>
-                        <input className="w-full  h-[50px] lg:h-[67px] text-sm  pl-5 lg:pl-8 pr-32 py-2 rounded-[35px] border hover:decoration-neutral outline-none text-white  lg:text-xl  border-violet-500 bg-transparent justify-between items-center inline-flex"
-                            type='email'
-                            placeholder='email'
+
+                <form className='flex flex-col w-full justify-center items-center lg:pt-[60px] pt-10 gap-0 lg:gap-5 ' onSubmit={handleSubmit(onSubmit)}>
+                    <div className='relative w-10/12 sm:w-7/12 md:w-5/12  lg:w-[600px]'>
+                        <Controller
+                            control={control}
+                            name="email"
+                            render={({ field: { onChange, value } }) => (
+                                <input className="w-full  h-[50px] lg:h-[67px] text-sm  pl-5 lg:pl-8 pr-32 py-2 rounded-[35px] border hover:decoration-neutral outline-none text-white  lg:text-xl  border-violet-500 bg-transparent justify-between items-center inline-flex"
+                                    type='email'
+                                    placeholder='email'
+                                    onChange={onChange}
+                                    value={value}
+                                />
+                            )}
                         />
                         <button className=" coming-soon-button-linear-gradient self-stretch px-5 lg:px-8 py-[10px] lg:py-3 bg-gradient-to-b from-violet-600 via-violet-700 to-violet-600 rounded-[40px] border border-neutral-600 justify-center items-center gap-2.5 flex
-                        text-white  text-sm leading-[14px] lg:text-xl font-normal  absolute top-[7px] lg:top-[6px] right-2">
-                            submit
+                        text-white  text-sm leading-[14px] lg:text-xl font-normal  absolute top-[7px] lg:top-[6px] right-2" type='submit'>
+                            {addEmailToWaitinListMutation.isPending ? <LoadingIcon /> : null} <span> submit </span>
                         </button>
                     </div>
                     <p className="text-[#B2AFD0E5] text-[10px] leading-[13px] pt-3 lg:pt-0  text-center  lg:text-lg font-light ">Sign up to find out when we launch</p>
 
-                </div>
+                </form>
             </div>
         </div>
     )
