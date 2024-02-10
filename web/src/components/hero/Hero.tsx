bc97d18e292cca3ff7b6a151faf02b0236f192b6
fortan <fortanpireva99@gmail.com>
2024-02-10 17:37:03 +0100

    make all sections animatable

--- web/src/components/hero/Hero.tsx
+++ web/src/components/hero/Hero.tsx
@@ -5,66 +5,50 @@ import Image from 'next/image'
 import PlayIcon from '@/assets/icons/PlayIcon.svg'
 import { siteCopy } from '@/config/site-config'
 import { motion } from 'framer-motion'
+import AnimatedSection from '../animated/AnimatedSection'
 export default function Hero() {
     return (
         <section
             className="my-20  mt-20 lg:mt-40   items-center text-center md:text-left text-black dark:text-white">
-            <motion.div className='grid grid-cols-1 lg:grid-cols-2 justify-center gap-x-8 gap-y-8'
-                initial={
-                    {
-                        opacity: 0,
-                        y: 100,
-                    }
-                }
-                animate={{
-                    opacity: 1,
-                    y: 0,
+            <AnimatedSection>
+                <div className='grid grid-cols-1 lg:grid-cols-2 justify-center gap-x-8 gap-y-8'>
 
-                }}
-                transition={
-                    {
-                        duration: 0.6,
-                        ease: 'easeOut'
-                    }
-                }
-            >
-                <div className="w-full flex flex-1 flex-col gap-4 ">
-                    <Typography type='h1' className=' text-left  text-stone-950 !font-black '>
-                        {siteCopy.heroSection.title}
-                    </Typography>
-                    <Typography type='p' className='text-left text-xl py-6 leading-[30px] font-normal text-neutral-600' >
-                        {siteCopy.heroSection.description}
-                    </Typography>
-                    <div className='flex justify-center flex-col lg:flex-row lg:justify-start gap-4 lg:gap-8'>
-                        <Button
-                            variant='primary'
-                            classname='py-3 px-4 bg-gradient-to-b  text-white'
-                            label='Call to action'
-                        />
-                        <Button
-                            variant='outline'
-                            label='Join Crash Course'
-                            icon={
-                                <div className='text-primary'>
+                    <div className="w-full flex flex-1 flex-col gap-4 ">
+                        <Typography type='h1' className=' text-left  text-stone-950 !font-black '>
+                            {siteCopy.heroSection.title}
+                        </Typography>
+                        <Typography type='p' className='text-left text-xl py-6 leading-[30px] font-normal text-neutral-600' >
+                            {siteCopy.heroSection.description}
+                        </Typography>
+                        <div className='flex justify-center flex-col lg:flex-row lg:justify-start gap-4 lg:gap-8'>
+                            <Button
+                                variant='primary'
+                                classname='py-3 px-4 bg-gradient-to-b  text-white'
+                                label='Call to action'
+                            />
+                            <Button
+                                variant='outline'
+                                label='Join Crash Course'
+                                icon={
+                                    <div className='text-primary'>
 
-                                    <PlayIcon />
-                                </div>
-                            }
+                                        <PlayIcon />
+                                    </div>
+                                }
+                            />
+                        </div>
+                    </div>
+                    <div className='relative w-full  flex-1 h-96'>
+                        <Image
+                            alt=''
+                            fill
+                            objectFit="cover"
+                            src={siteCopy.heroSection.image}
+                            className="w-full h-full object-cover rounded-2xl"
                         />
                     </div>
                 </div>
-                <div className='relative w-full  flex-1 h-96'>
-                    <Image
-                        alt=''
-                        fill
-                        objectFit="cover"
-                        src={siteCopy.heroSection.image}
-                        className="w-full h-full object-cover rounded-2xl"
-                    />
-                </div>
-            </motion.div>
-
+            </AnimatedSection>
         </section >
-
     )
 }
--- web/src/components/section/faq/FAQ.tsx
+++ web/src/components/section/faq/FAQ.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FaqItem from './FaqItem'
+import AnimatedSection from '@/components/animated/AnimatedSection'
 
 const faqItems = [
     {
@@ -22,17 +23,22 @@ const faqItems = [
 const FAQ = () => {
     const [showAnswer, setShowAnswer] = React.useState<number | null>(null)
     return (
-        <div className='container   my-32 flex flex-col justify-center items-center  w-full m-auto gap-20' >
-            <div className='text flex  flex-col justify-center items-center mb-8 md:mb-0 text-black dark:text-white  '>
-                <h1 className='text-5xl  font-bold mb-5'>FAQ</h1>
-                <h4 className='text-center text-xl '>Frequently Asked Questions</h4>
+        <AnimatedSection>
+
+            <div className='container   my-32 flex flex-col justify-center items-center  w-full m-auto gap-20' >
+                <div className='text flex  flex-col justify-center items-center mb-8 md:mb-0 text-black dark:text-white  '>
+                    <h1 className='text-5xl  font-bold mb-5'>FAQ</h1>
+                    <h4 className='text-center text-xl '>Frequently Asked Questions</h4>
+                </div>
+                <div className='faq-list flex flex-col justify-start gap-5  max-w-2xl w-full   '>
+                    {faqItems.map((item, index) => (
+                        <FaqItem question={item.question} answer={item.answer} isOpen={index == showAnswer} onClick={() => setShowAnswer(index == showAnswer ? null : index)} />
+
+                    ))}
+                </div>
             </div>
-            <div className='faq-list flex flex-col justify-start gap-5  max-w-2xl w-full   '>
-                {faqItems.map((item, index) => (
-                    <FaqItem key={index} question={item.question} answer={item.answer} isOpen={index == showAnswer} onClick={() => setShowAnswer(index == showAnswer ? null : index)} />
-                ))}
-            </div>
-        </div>
+        </AnimatedSection>
+
     )
 }
 
