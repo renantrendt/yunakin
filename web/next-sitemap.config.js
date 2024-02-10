9c7cdaea966c5490527183512c387d0bb43e1b19
fortan <fortanpireva99@gmail.com>
2024-02-10 10:31:34 +0100

    add sitemap in build phase

--- web/next-sitemap.config.js
+++ web/next-sitemap.config.js
@@ -1,8 +1,7 @@
-import platformConfig from "./config/app-config";
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: platformConfig.variables.NEXT_URL || 'http://localhost:3000/',
+    siteUrl: process.env.NEXT_URL || 'http://localhost:3000/',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }
