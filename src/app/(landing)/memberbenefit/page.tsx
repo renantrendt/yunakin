ec790ec7a20ec489ae040b13a6f1d368b28a235f
Renan Serrano <renan@visto.bio>
2024-06-22 14:05:32 -0700

    first class

---
+++ src/app/(landing)/memberbenefit/page.tsx
@@ -0,0 +1,53 @@
+import PageHeader from '@/components/blog/PageHeader';
+import React from 'react'
+import BlogCard from '@/components/blog/BlogCard';
+import getServerSideQueryClient from '@/react-query/server/queryClient';
+import { blogPostsWithCategoryQueryKey } from '@/react-query/queryKeys';
+import blogPostsWithCategoriesQueryFn from '@/react-query/server/queries/blogPostsWithCategories';
+export interface BlogsViewModel {
+    name: string;
+    slug: string;
+    articles: [
+        {
+            title: string;
+            description: any;
+            publishedAt: string;
+            imageURL: string;
+            short_description: string;
+            slug: string;
+            author: {
+                name: string;
+                avatar: string;
+            };
+        }
+    ]
+}
+
+
+const MemberbenefitPage = async () => {
+    const queryClient = await getServerSideQueryClient();
+
+    await queryClient.prefetchQuery(
+        {
+            queryKey: blogPostsWithCategoryQueryKey(),
+            queryFn: blogPostsWithCategoriesQueryFn,
+        }
+    )
+
+    const mappedData = await queryClient.getQueryData<BlogsViewModel[]>(
+        blogPostsWithCategoryQueryKey()
+    )
+
+    return (
+        <div className='max-w-[1440px] py-20 w-full mx-auto px-4 md:px-28'>
+            <div className='px-4 md:px-0'>
+                <PageHeader />
+            </div>
+            {mappedData && mappedData.map((category: BlogsViewModel, index: any) => (
+                <BlogCard loading={false} key={index} category={category} />
+            ))}
+        </div>
+    )
+}
+
+export default MemberbenefitPage
