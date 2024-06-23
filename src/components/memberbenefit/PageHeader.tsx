5523c95dc020117d64bb20043b00f34731374348
Renan Serrano <renan@visto.bio>
2024-06-23 11:22:52 -0700

    .

--- src/components/memberbenefit/PageHeader.tsx
+++ src/components/memberbenefit/PageHeader.tsx
@@ -4,7 +4,7 @@ import Typography from "../atomic/typography/Typography";
 export default function PageHeader() {
     return (
         <div className=' my-10 lg:my-20 flex flex-col justify-center items-center gap-3 lg:gap-5'>
-            <Typography type="h1" className="font-black text-[32px] leading-[45px] lg:text-5xl" >Member Benefits</Typography>
+            <Typography type="h1" className="font-black text-[32px] leading-[45px] lg:text-5xl" >Member Benefits.</Typography>
             <Typography type="p" className="text-base text-neutral-600 font-normal lg:text-xl" >Every month a new benefit to help you.</Typography>
         </div>
     );
