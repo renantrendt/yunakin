5ee36e7cc4c30501d616367dbf234f8b626de96f
Renan Serrano <renan@visto.bio>
2024-07-02 02:01:54 -0700

    changed buttons orders and variant

--- src/components/customize/CustomizePageContainer.tsx
+++ src/components/customize/CustomizePageContainer.tsx
@@ -119,18 +119,19 @@ const CustomizePageContainer = ({ benefits, categories, memberPageConfig }: Cust
             <ContentSection fullWidth className='flex fixed left-1/2 w-6/12 justify-between z-50' >
                 <div className='flex justify-end  gap-4 z-50 w-fit py-4 px-3'>
 
-                    <Button variant="secondary" className='w-full' label="View Live Page" onClick={() => {
+                    <Button variant="tertiary" className='w-full' label="View Live Page" onClick={() => {
                         const url = `${platformConfig.variables.NEXT_PUBLIC_NEXT_URL}/${config.clientSlug}/memberbenefits`
                         window.open(url, '_blank')
                     }} />
-                    <Button variant="primary" className='w-full' label="Publish Changes"
-                        onClick={publishChanges}
-                    />
-                    <Button variant="primary" className='w-full' label="Customize Color, Font and Domain"
+                    <Button variant="secondary" className='w-full' label="Customize Color, Font and Domain"
                         onClick={() => {
                             setSettingsModalOpen(true)
                         }}
                     />
+                    <Button variant="primary" className='w-full' label="Publish Changes"
+                        onClick={publishChanges}
+                    />
+
                 </div>
 
             </ContentSection >
