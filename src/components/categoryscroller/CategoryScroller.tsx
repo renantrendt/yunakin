9304a4150e20ccd4490b276082f6f353a4d0bd1f
fortan <fortanpireva99@gmail.com>
2024-07-10 10:04:54 +0200

    add category button scroller

---
+++ src/components/categoryscroller/CategoryScroller.tsx
@@ -0,0 +1,67 @@
+import { SelectedMemberBenefit } from '@/lib/types'
+import { Category } from '@prisma/client'
+import React from 'react'
+import SelectMemberBenefitCard from '../memberbenefit/SelectMemberBenefitCard'
+import { cn } from '@/utils/cn'
+import Button from '../atomic/button/Button'
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
+
+interface CategoryScrollerProps {
+    category: Category
+    selectedBenefits: SelectedMemberBenefit[]
+    setSelectedBenefits: (selectedBenefits: SelectedMemberBenefit[]) => void
+}
+
+const CategoryScroller = ({ category, selectedBenefits, setSelectedBenefits }: CategoryScrollerProps) => {
+    const [start, setStart] = React.useState(0)
+    const onNext = () => {
+        setStart(start + 1)
+        if (start === selectedBenefits.length - 1) {
+            setStart(0)
+        }
+    }
+    const onPrev = () => {
+        setStart(start - 1)
+        if (start === 0) {
+            setStart(selectedBenefits.length - 1)
+        }
+    }
+    return (
+
+        <div key={category.id} className='flex  flex-col gap-3 lg:gap-5 justify-start  mt-8 max-w-[100vw]   '>
+            <div className='flex justify-between items-center'>
+
+                <div className=" w-fit	  px-5 py-1.5 bg-category-blog-background rounded-[30px] border-none justify-start items-start gap-2.5 inline-flex dark:bg-card-dark">
+                    <div className=" text-center text-category-blog-color dark:text-sidebar-icon-dark text-sm font-semibold  uppercase tracking-[0.5px]">{category.name}</div>
+                </div>
+                <div className='justify-between flex w-fit gap-2'>
+                    <Button onClick={onPrev} variant='secondary' size='sm' icon={<ArrowLeftIcon />} className=' rounded-full bg-white  !w-fit  p-2 !min-w-fit  ' />
+                    <Button onClick={onNext} variant='secondary' size='sm' icon={<ArrowRightIcon />} className=' rounded-full bg-white !w-fit  p-2 !min-w-fit  ' />
+                </div>
+            </div>
+
+            <div className='relative w-full h-[400px]'>
+
+                <div className='  absolute   flex flex-row   gap-3  lg:gap-5 justify-between flex-nowrap      '>
+                    {selectedBenefits.map((benefit: SelectedMemberBenefit, index: any) => (
+                        // <BlogCard loading={false} key={index} category={category} />
+                        <div key={index} className={cn(' relative  min-w-[370px] duration-300 ease-in-out')} style={{ transform: `translateX(calc(-${start * 100}% - ${start * 24}px))` }}>
+
+                            <SelectMemberBenefitCard
+                                selected={benefit.selected}
+                                onClick={() => {
+                                    const newSelectedBenefits = selectedBenefits.map(b => b.id === benefit.id ? { ...b, selected: !b.selected } : b)
+                                    setSelectedBenefits(newSelectedBenefits)
+                                }}
+                                className='w-full'
+                                key={index} benefit={benefit} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default CategoryScroller
--- src/components/landing/onboarding/OnboardingContainer.tsx
+++ src/components/landing/onboarding/OnboardingContainer.tsx
@@ -21,8 +21,9 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { Input } from 'postcss'
 import React, { useState } from 'react'
-import { HamburgerMenuIcon, Cross1Icon } from '@radix-ui/react-icons'
+import { HamburgerMenuIcon, Cross1Icon, ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
 import GeneratePageModal from '@/components/molecules/generate-page-modal/GeneratePageModal'
+import CategoryScroller from '@/components/categoryscroller/CategoryScroller'
 
 interface OnboardingContainerProps {
     benefits: MemberBenefit[]
@@ -87,73 +88,64 @@ const OnboardingContainer = ({ benefits, categories }: OnboardingContainerProps)
 
                 </div>
             </ContentSection >
-            <div className='max-w-[1440px] py-20 w-full mx-auto px-4 md:px-28'>
-                <div className='px-4 md:px-0 lg:mt-40'>
-                    <div className=' my-10 lg:my-20 flex flex-col justify-center items-center gap-3 lg:gap-5 text-center'>
-                        <Typography type="h1" className="font-black" >{t(`step${step}.title`)}</Typography>
-                        <Typography type="h6" className="text-xl" >{t(`step${step}.description`)}</Typography>
-                    </div>
-                </div>
-                <div className='pt-8 lg:pt-20'>
-                    <div className="flex flex-col md:flex-row gap-4  items-start justify-between w-full  text-black md:items-center">
-                        <h1 className="text-xl lg:text-2xl font-bold">Perks</h1>
-                        <div className='tabs bg-[#F0F0F0]  p-1  w-full lg:w-fit flex flex-shrink-0 justify-between lg:justify-center  gap-2 rounded-[10px]'>
+            <div className='w-full overflow-hidden'>
 
-                            {Object.keys(selectMemberBenefitFilter).map((key: string, index) => {
-                                return (
-                                    <div key={index} className={`px-4  py-2 text-[#5E5E5E] w-full text-center font-satoshiBold font-medium text-sm lg:text-base rounded-lg cursor-pointer ${selectedDisplayType === selectMemberBenefitFilter[key as MemberBenefitFilter] ? 'bg-white' : ''}`} onClick={() => setSelectedDisplayType(selectMemberBenefitFilter[key as MemberBenefitFilter])}>
-                                        {selectMemberBenefitFilter[key as MemberBenefitFilter]}
-                                    </div>
-                                )
-                            })}
+                <div className='max-w-[1440px] py-20 w-full mx-auto px-4 md:px-28 '>
+                    <div className='px-4 md:px-0 lg:mt-40'>
+                        <div className=' my-10 lg:my-20 flex flex-col justify-center items-center gap-3 lg:gap-5 text-center'>
+                            <Typography type="h1" className="font-black" >{t(`step${step}.title`)}</Typography>
+                            <Typography type="h6" className="text-xl" >{t(`step${step}.description`)}</Typography>
                         </div>
                     </div>
-                    {step == 1 && (
-                        <div className='mb-40'>
-                            <div>
-                                {selectedDisplayType == selectMemberBenefitFilter.CATEGORY && categories.filter(category => selectedBenefits.filter(benefit => category.id == benefit.categoryId).length > 0).map((category) => {
+                    <div className='pt-8 lg:pt-20'>
+                        <div className="flex flex-col md:flex-row gap-4  items-start justify-between w-full  text-black md:items-center">
+                            <h1 className="text-xl lg:text-2xl font-bold">Perks</h1>
+                            <div className='tabs bg-[#F0F0F0]  p-1  w-full lg:w-fit flex flex-shrink-0 justify-between lg:justify-center  gap-2 rounded-[10px]'>
+
+                                {Object.keys(selectMemberBenefitFilter).map((key: string, index) => {
                                     return (
-                                        <div key={category.id} className='flex flex-col gap-3 lg:gap-5 justify-start  mt-8 overflow-x-scroll max-w-[100vw]  no-scrollbar '>
-                                            <div className=" w-fit	  px-5 py-1.5 bg-category-blog-background rounded-[30px] border-none justify-start items-start gap-2.5 inline-flex dark:bg-card-dark">
-                                                <div className=" text-center text-category-blog-color dark:text-sidebar-icon-dark text-sm font-semibold  uppercase tracking-[0.5px]">{category.name}</div>
-                                            </div>
-                                            <div className='flex flex-row gap-3 lg:gap-5 justify-items-center  overflow-x-scroll max-w-[100vw]  no-scrollbar '>
-                                                {selectedBenefits && selectedBenefits.filter((benefit: SelectedMemberBenefit) => benefit.categoryId === category.id).map((benefit: SelectedMemberBenefit, index: any) => (
-                                                    // <BlogCard loading={false} key={index} category={category} />
+                                        <div key={index} className={`px-4  py-2 text-[#5E5E5E] w-full text-center font-satoshiBold font-medium text-sm lg:text-base rounded-lg cursor-pointer ${selectedDisplayType === selectMemberBenefitFilter[key as MemberBenefitFilter] ? 'bg-white' : ''}`} onClick={() => setSelectedDisplayType(selectMemberBenefitFilter[key as MemberBenefitFilter])}>
+                                            {selectMemberBenefitFilter[key as MemberBenefitFilter]}
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                        {step == 1 && (
+                            <div className='mb-40'>
+                                <div>
+                                    {selectedDisplayType == selectMemberBenefitFilter.CATEGORY && categories.filter(category => selectedBenefits.filter(benefit => category.id == benefit.categoryId).length > 0).map((category) => {
+                                        return (
+                                            <CategoryScroller
+                                                category={category}
+                                                selectedBenefits={selectedBenefits.filter((benefit: SelectedMemberBenefit) => benefit.categoryId === category.id)}
+                                                setSelectedBenefits={setSelectedBenefits}
+                                            />
+                                        )
+                                    })}
+
+                                    {[selectMemberBenefitFilter.NEW, selectMemberBenefitFilter.FEATURED].includes(selectedDisplayType) && (
+                                        <div className='grid grid-cols-1  justify-items-stretch lg:justify-items-center md:grid-cols-2 lg:grid-cols-3  gap-x-5 gap-y-5 mt-8 '>
+                                            {selectedBenefits && selectedBenefits
+                                                .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+                                                .map((benefit: SelectedMemberBenefit, index: any) => (
                                                     <SelectMemberBenefitCard
                                                         selected={benefit.selected}
                                                         onClick={() => {
                                                             const newSelectedBenefits = selectedBenefits.map(b => b.id === benefit.id ? { ...b, selected: !b.selected } : b)
                                                             setSelectedBenefits(newSelectedBenefits)
                                                         }}
-                                                        className='w-11/12'
                                                         key={index} benefit={benefit} />
                                                 ))}
-                                            </div>
                                         </div>
-                                    )
-                                })}
-
-                                {[selectMemberBenefitFilter.NEW, selectMemberBenefitFilter.FEATURED].includes(selectedDisplayType) && (
-                                    <div className='grid grid-cols-1  justify-items-stretch lg:justify-items-center md:grid-cols-2 lg:grid-cols-3  gap-x-5 gap-y-5 mt-8 '>
-                                        {selectedBenefits && selectedBenefits
-                                            .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-                                            .map((benefit: SelectedMemberBenefit, index: any) => (
-                                                <SelectMemberBenefitCard
-                                                    selected={benefit.selected}
-                                                    onClick={() => {
-                                                        const newSelectedBenefits = selectedBenefits.map(b => b.id === benefit.id ? { ...b, selected: !b.selected } : b)
-                                                        setSelectedBenefits(newSelectedBenefits)
-                                                    }}
-                                                    key={index} benefit={benefit} />
-                                            ))}
-                                    </div>
-                                )}
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    )}
+                        )}
 
+                    </div>
                 </div>
+
                 {showOnboardingModal && (
                     <OnboardingModal
                         onClose={() => {
