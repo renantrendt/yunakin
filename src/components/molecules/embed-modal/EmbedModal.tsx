59e58494e052b05644601ce6a7d9abbd1d084408
fortan <fortanpireva99@gmail.com>
2024-07-23 21:43:46 +0200

    small changes in running scripts

--- src/components/molecules/embed-modal/EmbedModal.tsx
+++ src/components/molecules/embed-modal/EmbedModal.tsx
@@ -20,6 +20,7 @@ interface EmbedModalProps {
 }
 
 
+const domain = process.env.NEXT_PUBLIC_NEXT_URL
 
 const EmbedModal = ({ clientSlug, isOpen, onClose, onUpdate, loading }: EmbedModalProps) => {
 
@@ -29,7 +30,7 @@ const EmbedModal = ({ clientSlug, isOpen, onClose, onUpdate, loading }: EmbedMod
     const [slug, setSlug] = React.useState(clientSlug)
     const content = `
         <div class="yunakin-iframe-container" style="width:100%">
-            <iframe id="yunakin_frame" src='https://yunakin.com/${clientSlug}/memberbenefits?embedded=true' width='100%' height='1000' frameborder='0' scrolling='no'></iframe>
+            <iframe id="yunakin_frame" src='${domain}/${clientSlug}/memberbenefits?embedded=true' width='100%' height='1000' frameborder='0' scrolling='no'></iframe>
         </div>
         <script data-client-slug="${clientSlug}" >
             window.addEventListener('message', (event) => {
@@ -143,13 +144,13 @@ const EmbedModal = ({ clientSlug, isOpen, onClose, onUpdate, loading }: EmbedMod
                             <InputField
                                 disabled
                                 id='shareurl'
-                                value={`https://yunakin.com/${clientSlug}/memberbenefits`}
+                                value={`${domain}/${clientSlug}/memberbenefits`}
                                 onChange={() => { }}
                                 name='shareurl'
                             />
                             <Button
                                 onClick={() => {
-                                    handleClick(`https://yunakin.com/${clientSlug}/memberbenefits`)
+                                    handleClick(`${domain}/${clientSlug}/memberbenefits`)
                                 }}
                                 icon={<CopyIcon />}
                                 variant='primary'
