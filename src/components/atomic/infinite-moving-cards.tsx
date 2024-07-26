0f68b943bc76209e10cf05e93fbdbb1e30808738
Renan Serrano <renan@visto.bio>
2024-07-26 02:00:55 -0700

    small text e logos home

--- src/components/atomic/infinite-moving-cards.tsx
+++ src/components/atomic/infinite-moving-cards.tsx
@@ -4,9 +4,16 @@ import React from "react";
 import CompanyLogo1 from '@/icons/company-icons/logo-ipsum1.svg'
 import CompanyLogo2 from '@/icons/company-icons/logo-ipsum2.svg'
 import CompanyLogo3 from '@/icons/company-icons/logo-ipsum3.svg'
+import CompanyLogo4 from '@/icons/company-icons/logo-ipsum4.svg'
+import CompanyLogo5 from '@/icons/company-icons/logo-ipsum5.svg'
+import CompanyLogo6 from '@/icons/company-icons/logo-ipsum6.svg'
+import CompanyLogo7 from '@/icons/company-icons/logo-ipsum7.svg'
+import CompanyLogo8 from '@/icons/company-icons/logo-ipsum8.svg'
+import CompanyLogo9 from '@/icons/company-icons/logo-ipsum9.svg'
+
 import ParallaxText from "../animated/ParallaxText";
 const items = [
-    CompanyLogo1, CompanyLogo2, CompanyLogo3
+    CompanyLogo1, CompanyLogo2, CompanyLogo3, CompanyLogo4, CompanyLogo5, CompanyLogo6, CompanyLogo7, CompanyLogo8, CompanyLogo9
 ]
 export const InfiniteMovingCards = () => {
     return (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,7 +112,7 @@ const config: Config = {
       },
       animation: {
         scroll:
-          "infinite-scroll var(--animation-duration, 20s) var(--animation-direction, forwards) linear infinite",
+          "infinite-scroll var(--animation-duration, 50s) var(--animation-direction, forwards) linear infinite",
         "meteor-effect": "meteor 5s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
